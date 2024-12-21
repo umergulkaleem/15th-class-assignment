0f403ba59a5949027ceba40d0814bdf8047da38f
umergulkaleem <umergulkaleem>
2024-12-21 17:26:07 +0500

    vercel error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import Link from "next/link";
 export default async function Home() {
   const url = await fetch("https://jsonplaceholder.typicode.com/todos");
   const response = await url.json();
-  // console.log(response);
   return (
     <main>
       {response.map((todos: any) => (

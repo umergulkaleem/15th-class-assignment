c13ad96b663f6d0308b7fe57653a32d2e76466ec
umergulkaleem <umergulkaleem>
2024-12-21 17:51:53 +0500

    vercel key error gpt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
 import Link from "next/link";
+
 export default async function Home() {
   const url = await fetch("https://jsonplaceholder.typicode.com/todos");
   const response = await url.json();
   return (
     <main>
       {response.map((todos: any, index: any) => (
-        <div className="text-2xl">
-          <Link href={`${todos.id}`}>
+        <div key={todos.id} className="text-2xl">
+          <Link href={`/${todos.id}`}>
             <h1>
-              &nbsp; &nbsp;<li key={index}> {todos.id}</li>
+              &nbsp; &nbsp;<li>{todos.id}</li>
             </h1>
           </Link>
         </div>

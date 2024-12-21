ea456ff66a89498f6ac2406776e2c4f8319cb113
umergulkaleem <umergulkaleem>
2024-12-21 17:28:12 +0500

    vercel error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default async function Home() {
   const response = await url.json();
   return (
     <main>
-      {response.map((todos: any) => (
+      {response.map((todos: any, key: any) => (
         <div className="text-2xl">
           <Link href={`${todos.id}`}>
             <h1>&nbsp; &nbsp; {todos.id}</h1>
